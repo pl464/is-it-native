21502d04ecd666a5f2c1cdb9abe5ab402c4dbf12
Henry Lee <pl464@scarletmail.rutgers.edu>
2020-07-03 14:04:48 -0400

    Attempting to add link to static About page

--- robin_scraper/urls.py
+++ robin_scraper/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index_not_used, name='pre_results'),
     path('<search_term>', views.index_used, name='results'),
+    path('about/', TemplateView.About.as_view(template_name="about.html")),
 ]
--- robin_scraper/views.py
+++ robin_scraper/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from robin_scraper.models import  Searcher
